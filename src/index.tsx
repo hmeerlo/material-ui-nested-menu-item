72fa3d0c4986f3d007d08e188f0869d0ec964eb1
Herman Meerlo <h.meerlo@scisports.com>
2021-09-22 16:04:39 +0200

    Fix scroll issue

--- src/index.tsx
+++ src/index.tsx
@@ -174,6 +174,9 @@ const NestedMenuItem = React.forwardRef<
         // Set pointer events to 'none' to prevent the invisible Popover div
         // from capturing events for clicks and hovers
         style={{pointerEvents: 'none'}}
+        PaperProps={{
+          style: {pointerEvents: 'auto'}
+        }}
         anchorEl={menuItemRef.current}
         anchorOrigin={{
           vertical: 'top',
@@ -191,9 +194,7 @@ const NestedMenuItem = React.forwardRef<
           setIsSubMenuOpen(false)
         }}
       >
-        <div ref={menuContainerRef} style={{pointerEvents: 'auto'}}>
-          {children}
-        </div>
+        <div ref={menuContainerRef}>{children}</div>
       </Menu>
     </div>
   )
